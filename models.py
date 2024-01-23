a62944493ef808e5f7e06eb783fd54a5fce2758d
Femarko <fenstermark7@gmail.com>
2024-01-23 04:41:15 +0100

    models.py modified: engine and Session created

---
+++ models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+POSTGRES_DSN = f"postgresql://adv:secret@127.0.0.1:5431/adv"
+engine = create_engine(POSTGRES_DSN)
+
+Session = sessionmaker(bind=engine)
+
+
+
