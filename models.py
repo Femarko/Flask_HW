b99220f7635d72dde6b0877341c5216745877e98
Femarko <fenstermark7@gmail.com>
2024-01-24 19:07:10 +0100

    models creation finished

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 from datetime import datetime
+from typing import List
 
-from sqlalchemy import create_engine, Integer, String, DateTime, func
-from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import create_engine, Integer, String, DateTime, func, ForeignKey
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column, relationship
 
 POSTGRES_DSN = f"postgresql://adv:secret@127.0.0.1:5431/adv"
 engine = create_engine(POSTGRES_DSN)
@@ -13,25 +14,29 @@ class Base(DeclarativeBase):
     pass
 
 
-class UserBase(Base):
+class AdvAuthor(Base):
 
-    __tablename__ = 'app_user'
+    __tablename__ = 'adv_author'
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(50), unique=True, index=True, nullable=False)
     email: Mapped[str] = mapped_column(String(50), unique=True, index=True, nullable=False)
     password_hash: Mapped[str] = mapped_column(nullable=False)
+    advs: Mapped[List["Adv"]] = relationship(back_populates="author")
 
 
-class AdvBase(Base):
 
-    __tablename__ = 'app_adv'
+class Adv(Base):
+
+    __tablename__ = 'adv'
 
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(200), unique=False, index=True, nullable=False)
-    description: Mapped[str] = mapped_column(index=True,nullable=False)
-    creation_date: Mapped[datetime] = mapped_column(DateTime)
-    owner: Mapped[str] = mapped_column(foreign_key=UserBase.id)
+    description: Mapped[str] = mapped_column(index=True, unique=False, nullable=False)
+    creation_date: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+    author_id: Mapped[str] = mapped_column(foreign_key=AdvAuthor.id)
+    author: Mapped["AdvAuthor"] = relationship(back_populates="advs")
 
 
+Base.metadata.create_all(bind=engine)
 
