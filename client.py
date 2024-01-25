0703fbc034d0f918974f0c2e7382aa0c75ca4855
Femarko <fenstermark7@gmail.com>
2024-01-25 16:58:21 +0100

    tried to create adv

--- client.py
+++ client.py
@@ -3,9 +3,9 @@ import requests
 response = requests.post(
     'http://127.0.0.1:5000/adv',
     json={
-        "title": "some title",
-        "description": "some description",
-        "author": "some author"
+        "title": "some title 3",
+        "description": "some description 3",
+        "author": "some author 3"
     }
 )
 print(response.status_code)
