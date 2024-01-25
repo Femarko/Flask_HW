36de6cccc333685418d660fe0d4593f4448e043c
Femarko <fenstermark7@gmail.com>
2024-01-25 05:45:33 +0100

    post method created and request failed

--- client.py
+++ client.py
@@ -1,5 +1,12 @@
 import requests
 
-response = requests.get('http://127.0.0.1:5000/hello_world')
+response = requests.post(
+    'http://127.0.0.1:5000/adv',
+    json={
+        "title": "some title",
+        "description": "some description",
+        "author": "some author"
+    }
+)
 print(response.status_code)
 print(response.text)
--- server.py
+++ server.py
@@ -1,18 +1,40 @@
 import flask
 from flask import jsonify, request, Response
 from flask.views import MethodView
+from flask import Response
 
+from models import Session, Adv
 
 adv = flask.Flask('adv')
 
 
+@adv.before_request
+def before_request():
+    session = Session()
+    request.session = session
+
+
+@adv.after_request
+def after_request(response: Response):
+    request.session.close()
+    return response
+
+
 class AdvView(MethodView):
+
+    @property
+    def session(self) -> Session:
+        return request.session
 
     def get(self, adv_id: int):
         pass
 
     def post(self):
-        pass
+        adv_data = request.json
+        new_adv = Adv(**adv_data)
+        self.session.add(new_adv)
+        self.session.commit()
+        return jsonify({'id': new_adv.id})
 
     def patch(self, adv_id: int):
         pass
@@ -24,8 +46,6 @@ class AdvView(MethodView):
 adv_view = AdvView.as_view("adv_view")
 adv.add_url_rule("/adv/<int:adv_id>", view_func=adv_view, methods=["GET", "PATCH", "DELETE"])
 adv.add_url_rule("/adv", view_func=adv_view, methods=["POST"])
-
-
 
 if __name__ == "__main__":
     adv.run(debug=True)
