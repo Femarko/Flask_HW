16fefb082b778a97f64203bafbf035f325ab190c
Femarko <fenstermark7@gmail.com>
2024-01-25 02:57:08 +0100

    Authors removed from models.py and server.py, HttpError removed from server.py

--- models.py
+++ models.py
@@ -14,18 +14,6 @@ class Base(DeclarativeBase):
     pass
 
 
-class AdvAuthor(Base):
-
-    __tablename__ = 'adv_author'
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(50), unique=True, index=True, nullable=False)
-    email: Mapped[str] = mapped_column(String(50), unique=True, index=True, nullable=False)
-    password_hash: Mapped[str] = mapped_column(nullable=False)
-    advs: Mapped[List["Adv"]] = relationship(back_populates="author")
-
-
-
 class Adv(Base):
 
     __tablename__ = 'adv'
@@ -34,8 +22,7 @@ class Adv(Base):
     title: Mapped[str] = mapped_column(String(200), unique=False, index=True, nullable=False)
     description: Mapped[str] = mapped_column(index=True, unique=False, nullable=False)
     creation_date: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
-    author_id: Mapped[str] = mapped_column(ForeignKey("adv_author.id"))
-    author: Mapped["AdvAuthor"] = relationship(back_populates="advs")
+    author: Mapped[str] = mapped_column(String(200))
 
 
 Base.metadata.create_all(bind=engine)
--- server.py
+++ server.py
@@ -6,12 +6,6 @@ from flask.views import MethodView
 adv = flask.Flask('adv')
 
 
-class HttpError(Exception):
-    def __init__(self, status_code: int, description: str):
-        self.status_code = status_code
-        self.description = description
-
-
 class AdvView(MethodView):
 
     def get(self, adv_id: int):
@@ -27,27 +21,10 @@ class AdvView(MethodView):
         pass
 
 
-class AuthorView(MethodView):
-
-    def get(self, adv_id: int):
-        pass
-
-    def post(self, adv_id: int):
-        pass
-
-    def patch(self, adv_id: int):
-        pass
-
-    def delete(self, adv_id: int):
-        pass
-
 adv_view = AdvView.as_view("adv_view")
 adv.add_url_rule("/adv/<int:adv_id>", view_func=adv_view, methods=["GET", "PATCH", "DELETE"])
 adv.add_url_rule("/adv", view_func=adv_view, methods=["POST"])
 
-author_view = AuthorView("author_view")
-adv.add_url_rule("author/<int:author_id>", view_func=author_view, methods=["GET", "PATCH", "DELETE"])
-adv.add_url_rule("/author", view_func=author_view, methods=["POST"])
 
 
 if __name__ == "__main__":
