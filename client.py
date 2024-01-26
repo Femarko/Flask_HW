56b8c41f10a902465743faf93dc9151d09b2b312
Femarko <fenstermark7@gmail.com>
2024-01-26 04:00:23 +0100

    looks like 5 entries created

--- client.py
+++ client.py
@@ -3,13 +3,13 @@ import requests
 # response = requests.post(
 #     'http://127.0.0.1:5000/adv',
 #     json={
-#         "title": "some title 1",
-#         "description": "some description 1",
-#         "author": "some author 1"
+#         "title": "some title 5",
+#         "description": "some description 5",
+#         "author": "some author 5"
 #     }
 # )
 
-response = requests.get('http://127.0.0.1:5000/adv/1')
+response = requests.get('http://127.0.0.1:5000/adv/5')
 
 print(response.status_code)
 print(response.text)
