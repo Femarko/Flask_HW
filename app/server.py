0e3e3387b6c282ee712ec2b359bc5d8f4b43268f
femarko <fenstermark7@gmail.com>
2024-08-29 10:19:24 +0200

    Package "app" added, modules "error_handler.py" and "urls.py" added

--- app/server.py
+++ app/server.py
@@ -2,23 +2,12 @@ import flask
 from flask import jsonify, request, Response
 from flask.views import MethodView
 from flask import Response
+
+from app.error_handler import HttpError
 from models import Session, Adv
 from sqlalchemy.exc import IntegrityError
 
-
 adv = flask.Flask('adv')
-
-
-class HttpError(Exception):
-    def __init__(self, status_code: int, description: str):
-        self.status_code = status_code
-        self.description = description
-
-@adv.errorhandler(HttpError)
-def error_handler(error):
-    response = jsonify({"error": error.description})
-    response.status_code = error.status_code
-    return response
 
 
 @adv.before_request
@@ -45,7 +34,7 @@ def add_adv(adv: Adv):
         request.session.add(adv)
         request.session.commit()
     except IntegrityError as err:
-        raise HttpError(409,"advertisement already exists")
+        raise HttpError(409, "advertisement already exists")
     return adv
 
 
@@ -67,14 +56,12 @@ class AdvView(MethodView):
             }
         )
 
-
     def post(self):
         adv_data = request.json
         new_adv = Adv(**adv_data)
         self.session.add(new_adv)
         self.session.commit()
         return jsonify({'id': new_adv.id})
-
 
     def patch(self, adv_id: int):
         adv = get_adv(adv_id)
@@ -86,15 +73,14 @@ class AdvView(MethodView):
             {
                 "modified data":
                     {
-                    "id": adv.id,
-                    "title": adv.title,
-                    "description": adv.description,
-                    "creation_date": adv.creation_date.isoformat(),
-                    "author": adv.author
+                        "id": adv.id,
+                        "title": adv.title,
+                        "description": adv.description,
+                        "creation_date": adv.creation_date.isoformat(),
+                        "author": adv.author
                     }
             }
         )
-
 
     def delete(self, adv_id: int):
         adv = get_adv(adv_id)
@@ -104,15 +90,11 @@ class AdvView(MethodView):
             "description": adv.description,
             "creation_date": adv.creation_date,
             "author": adv.author
-            }
+        }
         self.session.delete(adv)
         self.session.commit()
         return jsonify({"deleted": adv_params})
 
-
-adv_view = AdvView.as_view("adv_view")
-adv.add_url_rule("/adv/<int:adv_id>", view_func=adv_view, methods=["GET", "PATCH", "DELETE"])
-adv.add_url_rule("/adv", view_func=adv_view, methods=["POST"])
 
 if __name__ == "__main__":
     adv.run(debug=True)
